21f17666b89de66b1ce80dc240d9b72e53fe3a31
Mohd. Talha Farooqui <mohdtalhafarooqui@gmail.com>
2025-10-01 21:25:55 +0530

    Add files via upload

---
+++ 1. Learn the Basics/1.1 Things to Know/1.1.1 Input Output Statement/output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hey Strver"<<"Hey Raj";
+    cout<<"Hey Talha";
+    cout<<"Hey Usman"<<"\n"<<"Hey Trijit"<<endl<<"Hey Debopam";
+    return 0;
+}
