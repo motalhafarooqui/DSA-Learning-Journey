1b24503c2dee7edf3dc379747dafa5ed01fbff46
Mohd. Talha Farooqui <mohdtalhafarooqui@gmail.com>
2025-10-01 21:23:26 +0530

    Delete 1. Learn the Basics directory

--- 1. Learn the Basics/1.1 Things to Know/1.1.1 Input Output Statement/inputoutput.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int x, y;
-    cin>>x>>y;
-    cout<<"Value of x : "<< x << "\n" << "Value of y : "<< y;
-    return 0;
-}
--- 1. Learn the Basics/1.1 Things to Know/1.1.1 Input Output Statement/output.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout<<"Hey Strver"<<"Hey Raj";
-    cout<<"Hey Talha";
-    cout<<"Hey Usman"<<"\n"<<"Hey Trijit"<<endl<<"Hey Debopam";
-    return 0;
-}
