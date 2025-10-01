cc5cc8cf9f3212ba80ffabae8e15e02fa8732113
Mohd. Talha Farooqui <mohdtalhafarooqui@gmail.com>
2025-10-01 21:27:26 +0530

    Add files via upload

---
+++ 1. Learn the Basics/1.1 Things to Know/1.1.2 Datatype/datatype.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << "Value of n : " << n << "\n";
+
+    long int m;
+    cin >> m;
+    cout << "Value of m : " << m << "\n";
+
+    long long int o;
+    cin >> o;
+    cout << "Value of o : " << o << "\n";
+
+    float p;
+    cin >> p;
+    cout << "Value of p : " << p << "\n";
+
+    double d;
+    cin >> d;
+    cout << "Value of d : " << d << "\n";
+
+    string ch;
+    cin >> ch;
+    cout << ch << "\n";
+
+    string str;
+    getline(cin, str);
+    cout << str;
+
+    return 0;
+}
